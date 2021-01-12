580e2838417d708d022cfb78151fb2c76071e78e
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-01-12 14:38:52 +0100

    Remove usings

--- src/Vs.Simulation.Core/DES/PersonEvents/Death.cs
+++ src/Vs.Simulation.Core/DES/PersonEvents/Death.cs
@@ -1,7 +1,6 @@
 ﻿using SimSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Vs.Simulation.Core.Probabilities;
 using Vs.Simulation.Shared;
 using Vs.Simulation.Shared.Model;
