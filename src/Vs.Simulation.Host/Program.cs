71584577c43473d283fa94c663cdc1cbc96b208a
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-01-12 12:24:35 +0100

    Adding SignalR Host

---
+++ src/Vs.Simulation.Host/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Threading.Tasks;
+
+namespace Vs.Simulation.Host
+{
+    public static class Program
+    {
+        public static void RunServer(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Start();
+        }
+
+        public static void Main(string[] args)
+        {
+           
+        }
+
+        private static IWebHostBuilder CreateWebHostBuilder(string[] args) => WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+    }
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseRouting();
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapHub<Telemetry>("/telemetry");
+            });
+        }
+
+        public IConfiguration Configuration { get; }
+    }
+}
---
+++ src/Vs.Simulation.Host/Telemetry.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using Vs.Simulation.Shared.Model;
+
+namespace Vs.Simulation.Host
+{
+    internal class Telemetry : Hub
+    {
+
+
+        /// <summary>
+        /// Sends a single triple to one client or a group of clients.
+        /// </summary>
+        /// <param name="t"></param>
+        /// <param name="group"></param>
+        /// <returns></returns>
+        public Task SendTriple(Triple t, string group = null)
+        {
+            if (string.IsNullOrEmpty(group))
+            {
+                return Clients.Caller.SendAsync("ReceiveTriple", t);
+            }
+            else
+            {
+                return Clients.Group(group).SendAsync("ReceiveTriple", t);
+            }
+        }
+
+        public Task SendStatistics(Statistics statistics, string group = null)
+        {
+            if (string.IsNullOrEmpty(group))
+            {
+                return Clients.Caller.SendAsync("ReceiveStatistics", statistics);
+            }
+            else
+            {
+                return Clients.Group(group).SendAsync("ReceiveStatistics", statistics);
+            }
+        }
+    }
+}
