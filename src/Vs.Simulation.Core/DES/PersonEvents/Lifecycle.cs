94f38798209262f1643e59a182419b3beaf82721
Sjef van Leeuwen <sjef.van.leeuwen@live.nl>
2021-01-12 19:53:34 +0100

    class rename

--- src/Vs.Simulation.Core/DES/PersonEvents/Lifecycle.cs
+++ src/Vs.Simulation.Core/DES/PersonEvents/Lifecycle.cs
@@ -1,7 +1,6 @@
 ﻿using SimSharp;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Vs.Simulation.Core.Probabilities;
 using Vs.Simulation.Shared;
 using Vs.Simulation.Shared.Model;
@@ -12,44 +11,43 @@ namespace Vs.Simulation.Core
     {
         public partial class Person
         {
-            private IEnumerable<Event> Death()
+            private IEnumerable<Event> LifeCycle()
             {
-                yield return Environment.Timeout(_data.End);
-                Population.Events.Write(new Triple()
+                var idx = Convert.ToByte(_data.Flags[Constants.idx_gender]);
+                _data.End = TimeSpan.FromDays(Environment.RandChoice(
+                    AgeProbability.Source[idx, 0],
+                    AgeProbability.Weights[idx, 0]) * 365);
+                _data.Dod = _data.Dob + _data.End;
+                _data.YearDod = _data.Dod.Year;
+                Global._totalAge[idx] += _data.End.Days / 365;
+                if (_data.parents != null)
+                    populationGrowth.AddPerson(this);
+
+                Environment.Process(Death());
+                // Only Schedule the next process chain if the person is expected to reach maturity.
+                if (_data.End.Days > 18 * 365)
                 {
-                    Subject = _data.Id,
-                    Predicate = Constants.triple_predicate_died_of,
-                    Object = _data.Id,
-                    Time = Environment.Now
-                });
-                Statistics.Deaths++;
-
-                //If the person was married, make the partner a widow and available for marriage again.
-                if (_data.Flags[Constants.idx_married] == true)
-                {
-
-                    _data.Flags[Constants.idx_married] = false;
-                    _data.partners.Last().Object._data.Flags[Constants.idx_married] = false;
-                    _data.partners.Last().Object._data.partners.Last().End = Environment.Now;
-                    _data.partners.Last().End = Environment.Now;
-
-                    //Checks if the widowed partner will remarry
-                    var idx = Convert.ToByte(_data.partners.Last().Object._data.Flags[Constants.idx_gender]);
-                    if (Environment.RandChoice(MaritalStatusProbability.MaritalAgeSource,
-                                MaritalStatusProbability.MaritalAgeWeights[idx]) == 1)
+                    Statistics.ReachMaturity++; // indicating this person will reach maturity.
+                    // Wait for adulthood.
+                    yield return Environment.Timeout(TimeSpan.FromDays(18 * 365));
+                    Events.Write(new Triple() { Subject = _data.Id, Predicate = Constants.triple_predicate_adult, Time = Environment.Now, Object = _data.Id });
+                    // put this person in the unmarried queue
+                    Unmarried[Convert.ToByte(_data.Flags[Constants.idx_gender])].Push(this);
+                    // determine if this person will ever initiate a marriage process.
+                    var status = Environment.RandChoice(MaritalStatusProbability.MaritalStatusSource,
+                        MaritalStatusProbability.MaritalStatusWeights[Environment.Now.Year - 1950]);
+                    if (status != Constants.marital_single)
                     {
-                        Remarry[Convert.ToByte(_data.Flags[Constants.idx_gender])].Push(_data.partners.Last().Object);
+                        // only people who do not stay single in their life cycle will be scheduled to marry.
+                        var maritalAge = TimeSpan.FromDays(Environment.RandChoice(MaritalStatusProbability.MaritalAgeSource,
+                            MaritalStatusProbability.MaritalAgeWeights[idx])) * 365;
+                        // only people who do not die before the marital age are scheduled to marry.
+                        if (maritalAge < _data.End)
+                            Environment.Process(Marry(maritalAge));
                     }
-
-                    Population.Events.Write(new Triple()
-                    {
-                        Subject = _data.partners.Last().Object._data.Id,
-                        Predicate = Constants.triple_predicate_widow_of,
-                        Object = _data.Id,
-                        Time = Environment.Now
-                    });
                 }
             }
+
         }
     }
 }
